a178b321d4aee8f2f673c608a8f1add6fe3502b2
Matthias Buehl <buehl@sequoiacap.com>
2017-11-11 11:57:45 -0800

    convert Book to functional component

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,31 @@
 import React from 'react'
 
-class Book extends React.Component {
-  render() {
-    const { book, handleShelfChange } = this.props
+const Book = ({ book, handleShelfChange }) => {
     const { title, authors, imageLinks, shelf } = book
 
-    return (
-      <div className="book">
-      <div className="book-top">
-        <div
-          className="book-cover" style={{
-          width: 128,
-          height: 193,
-          backgroundImage: imageLinks && `url(${imageLinks["thumbnail"]}`
-        }}></div>
-        <div className="book-shelf-changer">
-          <select value={ shelf || 'none' } onChange={ e => handleShelfChange(book, e.target.value) }>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="remove">None</option>
-          </select>
-        </div>
+  return (
+    <div className="book">
+    <div className="book-top">
+      <div
+        className="book-cover" style={{
+        width: 128,
+        height: 193,
+        backgroundImage: imageLinks && `url(${imageLinks["thumbnail"]}`
+      }}></div>
+      <div className="book-shelf-changer">
+        <select value={ shelf || 'none' } onChange={ e => handleShelfChange(book, e.target.value) }>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="remove">None</option>
+        </select>
       </div>
-      <div className="book-title">{ title }</div>
-      <div className="book-authors">{ authors }</div>
     </div>
-    )
-  }
+    <div className="book-title">{ title }</div>
+    <div className="book-authors">{ authors }</div>
+  </div>
+  )
 }
 
 export default Book
