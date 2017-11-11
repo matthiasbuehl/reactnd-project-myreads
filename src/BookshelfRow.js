a989a5939ff0d3cce4b2efb95fd977be5864b51b
Matthias Buehl <buehl@sequoiacap.com>
2017-11-11 11:55:06 -0800

    convert BookshelfRow to functional component

--- src/BookshelfRow.js
+++ src/BookshelfRow.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import Book from './Book'
 
-class BookshelfRow extends React.Component {
-  render() {
-    const { books, handleShelfChange } = this.props
-
-    return (
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-          { books.map( book =>
-            <li key={ book.id }>
-              <Book
-                book={ book }
-                handleShelfChange={ handleShelfChange }
-              />
-            </li>
-          )}
-        </ol>
-      </div>
-    )
-  }
+const BookshelfRow = ({books, handleShelfChange}) => {
+  return (
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        { books.map( book =>
+          <li key={ book.id }>
+            <Book
+              book={ book }
+              handleShelfChange={ handleShelfChange }
+            />
+          </li>
+        )}
+      </ol>
+    </div>
+  )
 }
 
 export default BookshelfRow
