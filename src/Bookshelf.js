51c17729e8d1c63853db76375dca36dcc41a589f
Matthias Buehl <buehl@sequoiacap.com>
2017-11-11 11:52:48 -0800

    convert Booshelf to functional component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,20 +1,16 @@
 import React from 'react'
 import BookshelfRow from './BookshelfRow'
 
-class Bookshelf extends React.Component {
-  render() {
-    const { name, handleShelfChange } = this.props
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{ name }</h2>
-        <BookshelfRow
-          books={ this.props.books }
-          handleShelfChange={ handleShelfChange }
-        />
-      </div>
-    )
-  }
+const Bookshelf = ({ name, books, handleShelfChange }) => {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{ name }</h2>
+      <BookshelfRow
+        books={ books }
+        handleShelfChange={ handleShelfChange }
+      />
+    </div>
+  )
 }
 
 export default Bookshelf
